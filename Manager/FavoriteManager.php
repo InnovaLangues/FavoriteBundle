b7634e79a05f119900b4ca03dce261cb44ca098b
Elorfin <penin.axel@gmail.com>
2014-01-28 15:58:24 +0100

    Get only Shortcuts created by current user

--- Manager/FavoriteManager.php
+++ Manager/FavoriteManager.php
@@ -8,42 +8,43 @@ use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class FavoriteManager
 {
-	/**
-	 * Object Manager
-	 * @var \Doctrine\Common\Persistence\ObjectManager
-	 */
-	protected $om;
-	
-	/**
-	 * Security context
-	 * @var \Symfony\Component\Security\Core\SecurityContextInterface
-	 */
-	protected $securityContext;
-	
-	/**
-	 * Class constructor
-	 * @param \Doctrine\Common\Persistence\ObjectManager                $objectManager
-	 * @param \Symfony\Component\Security\Core\SecurityContextInterface $securityContext
-	 */
-	public function __construct(
-		ObjectManager            $objectManager,
-		SecurityContextInterface $securityContext
-	) {
-		$this->om              = $objectManager;
-		$this->securityContext = $securityContext;
-	}
-	
-	public function getUserFavorites(User $user = null)
-	{
-		if (empty($user)) {
-			$user = $this->securityContext->getToken()->getUser();
-		}
-		
-// 		$shortcutType = $this->om->getRepository('ClarolineCoreBundle:Resource\ResourceType')->findOneByName('resource_shortcut');
-		
-		// Get all shortcuts of user
-		$favorites = $this->om->getRepository('ClarolineCoreBundle:Resource\ResourceNode')->findBy(array ('class' => 'Claroline\CoreBundle\Entity\Resource\ResourceShortcut'));
-		
-		return $favorites;
-	}
+    /**
+     * Object Manager
+     * @var \Doctrine\Common\Persistence\ObjectManager
+     */
+    protected $om;
+    
+    /**
+     * Security context
+     * @var \Symfony\Component\Security\Core\SecurityContextInterface
+     */
+    protected $securityContext;
+    
+    /**
+     * Class constructor
+     * @param \Doctrine\Common\Persistence\ObjectManager                $objectManager
+     * @param \Symfony\Component\Security\Core\SecurityContextInterface $securityContext
+     */
+    public function __construct(
+        ObjectManager            $objectManager,
+        SecurityContextInterface $securityContext
+    ) {
+        $this->om              = $objectManager;
+        $this->securityContext = $securityContext;
+    }
+    
+    public function getUserFavorites(User $user = null)
+    {
+        if (empty($user)) {
+            $user = $this->securityContext->getToken()->getUser();
+        }
+        
+        // Get all shortcuts of user
+        $favorites = $this->om->getRepository('ClarolineCoreBundle:Resource\ResourceNode')->findBy(array (
+            'class' => 'Claroline\CoreBundle\Entity\Resource\ResourceShortcut',
+            'creator' => $user,
+        ));
+    
+        return $favorites;
+    }
 }
